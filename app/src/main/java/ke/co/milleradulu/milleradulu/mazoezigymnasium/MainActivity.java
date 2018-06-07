c6d77724627a896fd046aafe6f3c52a1bdbb5a7a
Miller Adulu <milleradulu@gmail.com>
2018-06-08 00:34:43 +0300

    Load profile information from the API

--- app/src/main/java/ke/co/milleradulu/milleradulu/mazoezigymnasium/MainActivity.java
+++ app/src/main/java/ke/co/milleradulu/milleradulu/mazoezigymnasium/MainActivity.java
@@ -1,7 +1,9 @@
 package ke.co.milleradulu.milleradulu.mazoezigymnasium;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void openProfile(View view) {
+        Intent profileIntent = new Intent(this, ProfileActivity.class);
+        MainActivity.this.startActivity(profileIntent);
     }
 }
---
+++ app/src/main/java/ke/co/milleradulu/milleradulu/mazoezigymnasium/ProfileActivity.java
@@ -0,0 +1,77 @@
+package ke.co.milleradulu.milleradulu.mazoezigymnasium;
+
+import android.content.Context;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class ProfileActivity extends AppCompatActivity {
+
+    TextView firstName, lastName, email, age, gender, weight, target_weight;
+    public static final String MAZOEZI_URL = "https://mazoezigymnasium.herokuapp.com/member/profile/2";
+    public static final String TAG = "PROFILE SCREEN";
+    RequestQueue profileRequestQueue;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+
+        firstName = findViewById(R.id.first_name);
+        lastName = findViewById(R.id.last_name);
+        email = findViewById(R.id.email);
+        age = findViewById(R.id.age);
+        gender = findViewById(R.id.gender);
+        weight = findViewById(R.id.weight);
+        target_weight = findViewById(R.id.target_weight);
+
+        profileRequestQueue = Volley.newRequestQueue(this);
+
+        JsonObjectRequest profileJSONObjectRequest = new JsonObjectRequest(
+                Request.Method.GET,
+                MAZOEZI_URL,
+                null,
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        try{
+                            JSONObject profileObject = response.getJSONObject("data");
+
+                            Log.d(TAG, profileObject.toString());
+
+                            firstName.setText(profileObject.getString("first_name"));
+                            lastName.setText(profileObject.getString("last_name"));
+                            email.setText(profileObject.getString("email"));
+                            age.setText(profileObject.getString("age"));
+                            gender.setText(profileObject.getString("gender"));
+                            weight.setText(profileObject.getString("weight"));
+                            target_weight.setText(profileObject.getString("target_weight"));
+
+                        } catch (JSONException e){
+                            e.printStackTrace();
+                        }
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Log.d(TAG, error.toString());
+                    }
+                }
+        );
+
+        profileRequestQueue.add(profileJSONObjectRequest);
+
+    }
+}
