b1015d9fa2dddbba8ee0864fcbdda1c94495821d
Miller Adulu <milleradulu@gmail.com>
2018-06-08 02:53:20 +0300

    Gym Instructor data model

---
+++ app/src/main/java/ke/co/milleradulu/milleradulu/mazoezigymnasium/GymInstructor.java
@@ -0,0 +1,29 @@
+package ke.co.milleradulu.milleradulu.mazoezigymnasium;
+
+public class GymInstructor {
+    private String names, email, gender;
+
+    public String getNames() {
+        return names;
+    }
+
+    public void setNames(String names) {
+        this.names = names;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+}
