1ae8acb95511bf4e81e9996d0e9eda54ddf4acc3
Miller Adulu <milleradulu@gmail.com>
2018-06-09 02:36:23 +0300

    Gym location Model

---
+++ app/src/main/java/ke/co/milleradulu/milleradulu/mazoezigymnasium/gymlocations/GymLocation.java
@@ -0,0 +1,29 @@
+package ke.co.milleradulu.milleradulu.mazoezigymnasium.gymlocations;
+
+public class GymLocation {
+    private String gymLocation, gymOpeningTime, gymClosingTime;
+
+    public String getGymLocation() {
+        return gymLocation;
+    }
+
+    public void setGymLocation(String gymLocation) {
+        this.gymLocation = gymLocation;
+    }
+
+    public String getGymOpeningTime() {
+        return gymOpeningTime;
+    }
+
+    public void setGymOpeningTime(String gymOpeningTime) {
+        this.gymOpeningTime = gymOpeningTime;
+    }
+
+    public String getGymClosingTime() {
+        return gymClosingTime;
+    }
+
+    public void setGymClosingTime(String gymClosingTime) {
+        this.gymClosingTime = gymClosingTime;
+    }
+}
