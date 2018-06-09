e385a78adb2c72f0b4f6e6ceefec420116087e15
Miller Adulu <milleradulu@gmail.com>
2018-06-09 04:26:42 +0300

    Work Out Model

---
+++ app/src/main/java/ke/co/milleradulu/milleradulu/mazoezigymnasium/workoutsessions/history/WorkOut.java
@@ -0,0 +1,45 @@
+package ke.co.milleradulu.milleradulu.mazoezigymnasium.workoutsessions.history;
+
+public class WorkOut {
+    private String date, location, exercise, reps, sets;
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public String getExercise() {
+        return exercise;
+    }
+
+    public void setExercise(String exercise) {
+        this.exercise = exercise;
+    }
+
+    public String getReps() {
+        return reps;
+    }
+
+    public void setReps(String reps) {
+        this.reps = reps;
+    }
+
+    public String getSets() {
+        return sets;
+    }
+
+    public void setSets(String sets) {
+        this.sets = sets;
+    }
+}
