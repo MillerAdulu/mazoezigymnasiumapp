95c6af9436e1e860ee1a907bbb2cdc74e0a5a69f
Miller Adulu <milleradulu@gmail.com>
2018-06-15 12:28:21 +0300

    Moved for use by other classes

---
+++ app/src/main/java/ke/co/milleradulu/milleradulu/mazoezigymnasium/workoutsessions/WorkOut.java
@@ -0,0 +1,45 @@
+package ke.co.milleradulu.milleradulu.mazoezigymnasium.workoutsessions;
+
+public class WorkOut {
+    private String date, location, exercise_type, reps, sets;
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public String getExercise_type() {
+        return exercise_type;
+    }
+
+    public void setExercise_type(String exercise_type) {
+        this.exercise_type = exercise_type;
+    }
+
+    public String getReps() {
+        return reps;
+    }
+
+    public void setReps(String reps) {
+        this.reps = reps;
+    }
+
+    public String getSets() {
+        return sets;
+    }
+
+    public void setSets(String sets) {
+        this.sets = sets;
+    }
+}
