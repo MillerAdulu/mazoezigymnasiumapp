7f4a0739204c371fd9842c4d2c9279c17624239b
Miller Adulu <milleradulu@gmail.com>
2018-07-03 18:41:02 +0300

    Better indenting

--- app/src/main/java/ke/co/milleradulu/milleradulu/mazoezigymnasium/APIServiceProvider.java
+++ app/src/main/java/ke/co/milleradulu/milleradulu/mazoezigymnasium/APIServiceProvider.java
@@ -6,24 +6,24 @@ import retrofit2.converter.gson.GsonConverterFactory;
 
 
 public class APIServiceProvider {
-    private static final String BASE_URL = "https://mazoezigymnasium.herokuapp.com";
+  private static final String BASE_URL = "https://mazoezigymnasium.herokuapp.com";
 
-    private static OkHttpClient.Builder httpClient = new OkHttpClient.Builder();
+  private static OkHttpClient.Builder httpClient = new OkHttpClient.Builder();
 
-    private static Retrofit.Builder builder =
-            new Retrofit.Builder()
-                    .baseUrl(BASE_URL)
-                    .addConverterFactory(
-                            GsonConverterFactory.create()
-                    );
+  private static Retrofit.Builder builder =
+    new Retrofit.Builder()
+      .baseUrl(BASE_URL)
+      .addConverterFactory(
+        GsonConverterFactory.create()
+      );
 
-    private static Retrofit retrofit = builder.client(
-            httpClient.build()
-    ).build();
+  private static Retrofit retrofit = builder.client(
+    httpClient.build()
+  ).build();
 
-    public static <S> S createService(
-            Class<S> serviceClass
-    ) {
-        return retrofit.create(serviceClass);
-    }
+  public static <S> S createService(
+    Class<S> serviceClass
+  ) {
+    return retrofit.create(serviceClass);
+  }
 }
