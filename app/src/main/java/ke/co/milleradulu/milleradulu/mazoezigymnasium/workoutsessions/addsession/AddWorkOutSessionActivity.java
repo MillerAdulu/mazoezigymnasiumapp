1e58914466961310b42a9d5eb75d837d073470fc
Miller Adulu <milleradulu@gmail.com>
2018-07-03 18:52:41 +0300

    Better indenting

--- app/src/main/java/ke/co/milleradulu/milleradulu/mazoezigymnasium/workoutsessions/addsession/AddWorkOutSessionActivity.java
+++ app/src/main/java/ke/co/milleradulu/milleradulu/mazoezigymnasium/workoutsessions/addsession/AddWorkOutSessionActivity.java
@@ -12,12 +12,15 @@ import android.view.inputmethod.InputMethodManager;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
+import android.widget.ProgressBar;
 import android.widget.Spinner;
 import android.widget.Toast;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
+import ke.co.milleradulu.milleradulu.mazoezigymnasium.SessionManager;
 import ke.co.milleradulu.milleradulu.mazoezigymnasium.models.Exercise;
 import ke.co.milleradulu.milleradulu.mazoezigymnasium.clients.ExerciseClient;
 import ke.co.milleradulu.milleradulu.mazoezigymnasium.R;
@@ -32,184 +35,199 @@ import retrofit2.Response;
 
 public class AddWorkOutSessionActivity extends AppCompatActivity {
 
-    private static final String TAG = AddWorkOutSessionActivity.class.getSimpleName();
+  private static final String TAG = AddWorkOutSessionActivity.class.getSimpleName();
+  SessionManager sessionManager;
 
-    private int location;
-    private int year;
-    private int month;
-    private int day;
-    private int exercise_type;
-    EditText addSets, addReps, sessionDate;
-    List<GymLocation> gymLocationsList;
-    List<Exercise> exerciseTypesList;
-    Spinner sessionLocationsSpinner, exerciseTypeSpinner;
+  private int location;
+  private int year;
+  private int month;
+  private int day;
+  private int member;
+  ProgressBar loading;
 
-    List<String> locations = new ArrayList<>();
-    List<String> exercises = new ArrayList<>();
-    ArrayAdapter<String> locationsAdapter, exercisesAdapter;
+  HashMap<String, String> memberData;
+  private int exercise_type;
+  EditText addSets, addReps, sessionDate;
+  List<GymLocation> gymLocationsList;
+  List<Exercise> exerciseTypesList;
+  Spinner sessionLocationsSpinner, exerciseTypeSpinner;
 
-    ArrayMap<String, Integer> locationMap = new ArrayMap<>();
-    ArrayMap<String, Integer> exerciseMap = new ArrayMap<>();
+  List<String> locations = new ArrayList<>();
+  List<String> exercises = new ArrayList<>();
+  ArrayAdapter<String> locationsAdapter, exercisesAdapter;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_add_work_out_session);
+  ArrayMap<String, Integer> locationMap = new ArrayMap<>();
+  ArrayMap<String, Integer> exerciseMap = new ArrayMap<>();
 
-        sessionLocationsSpinner = findViewById(R.id.location_spinner);
-        exerciseTypeSpinner = findViewById(R.id.exercise_type_spinner);
-        addSets = findViewById(R.id.add_sets);
-        addReps = findViewById(R.id.add_reps);
-        sessionDate = findViewById(R.id.editTextDate);
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_add_work_out_session);
 
-        loadLocationsSpinner();
-        loadExercisesSpinner();
-    }
+    sessionManager = new SessionManager(getApplicationContext());
+    sessionManager.checkLogin();
 
-    public void showDatePickerDialog(View view) {
-        DialogFragment dialogFragment = new DatePickerFragment();
-        dialogFragment.show(getFragmentManager(), getString(R.string.pick_date));
-    }
+    sessionLocationsSpinner = findViewById(R.id.location_spinner);
+    exerciseTypeSpinner = findViewById(R.id.exercise_type_spinner);
+    addSets = findViewById(R.id.add_sets);
+    addReps = findViewById(R.id.add_reps);
+    loading = findViewById(R.id.add_session_load);
+    sessionDate = findViewById(R.id.editTextDate);
+    memberData = sessionManager.getMemberDetails();
+    member = Integer.parseInt(
+      memberData.get(
+        SessionManager.KEY_MEMBER_ID
+      )
+    );
+    loading.setVisibility(View.VISIBLE);
+    loadLocationsSpinner();
+    loadExercisesSpinner();
+  }
 
-    public void processDatePickerResult(int year, int month, int day) {
-        this.year = year;
-        this.month = month;
-        this.day = day;
+  public void showDatePickerDialog(View view) {
+    DialogFragment dialogFragment = new DatePickerFragment();
+    dialogFragment.show(getFragmentManager(), getString(R.string.pick_date));
+  }
 
-        sessionDate.setText(
-                new StringBuilder()
-                        .append(day)
-                        .append("/")
-                .append(month + 1)
-                .append("/")
-                .append(year)
+  public void processDatePickerResult(int year, int month, int day) {
+    this.year = year;
+    this.month = month;
+    this.day = day;
 
-        );
-    }
+    sessionDate.setText(
+      new StringBuilder()
+        .append(day)
+        .append("/")
+        .append(month + 1)
+        .append("/")
+        .append(year)
 
-    public void addSession(View view) {
-        hideKeyboard();
-        int sets = Integer.parseInt(addSets.getText().toString());
-        int reps = Integer.parseInt(addReps.getText().toString());
+    );
+  }
 
-        WorkOutSessionClient workOutSessionClient = APIServiceProvider.createService(WorkOutSessionClient.class);
-        int member = 1;
-        Call<WorkOut> workOutCall = workOutSessionClient.addSession(
-                year,
-                month,
-                day,
-                location,
-                exercise_type,
-                reps,
-                sets,
-                member
-        );
-        workOutCall.enqueue(new Callback<WorkOut>() {
-            @Override
-            public void onResponse(@NonNull Call<WorkOut> call, @NonNull Response<WorkOut> response) {
-                if(response.isSuccessful()) {
-                    Toast.makeText(AddWorkOutSessionActivity.this, "Session Added Successfully", Toast.LENGTH_SHORT).show();
-                }
-            }
+  public void addSession(View view) {
+    hideKeyboard();
+    int sets = Integer.parseInt(addSets.getText().toString());
+    int reps = Integer.parseInt(addReps.getText().toString());
 
-            @Override
-            public void onFailure(@NonNull Call<WorkOut> call, @NonNull Throwable t) {
-                Log.d(TAG, t.toString());
-            }
-        });
-    }
-
-    public void loadLocationsSpinner() {
-        GymLocationClient gymLocationClient = APIServiceProvider.createService(GymLocationClient.class);
-        Call<List<GymLocation>> gymLocationCall = gymLocationClient.gymLocations();
-
-        gymLocationCall.enqueue(new Callback<List<GymLocation>>() {
-            @Override
-            public void onResponse(@NonNull Call<List<GymLocation>> call, @NonNull Response<List<GymLocation>> response) {
-                gymLocationsList = response.body();
-                addLocationsToArrayList();
-                locationsSpinnerListener();
-            }
-
-            @Override
-            public void onFailure(@NonNull Call<List<GymLocation>> call, @NonNull Throwable t) {
-                Log.d(TAG, t.toString());
-            }
-        });
-    }
-
-    public void loadExercisesSpinner() {
-        ExerciseClient exerciseClient = APIServiceProvider.createService(ExerciseClient.class);
-        Call<List<Exercise>> exercisesCall = exerciseClient.exercises();
-
-        exercisesCall.enqueue(new Callback<List<Exercise>>() {
-            @Override
-            public void onResponse(@NonNull Call<List<Exercise>> call, @NonNull Response<List<Exercise>> response) {
-                exerciseTypesList = response.body();
-                addExercisesToArrayList();
-                exercisesSpinnerListener();
-            }
-
-            @Override
-            public void onFailure(@NonNull Call<List<Exercise>> call, @NonNull Throwable t) {
-                Log.d(TAG, t.toString());
-            }
-        });
-    }
-
-    public void addLocationsToArrayList() {
-
-        for (GymLocation gymLocation : gymLocationsList ) {
-            locations.add(gymLocation.getLocation());
-            locationMap.put(gymLocation.getLocation(), gymLocation.getId());
+    WorkOutSessionClient workOutSessionClient = APIServiceProvider.createService(WorkOutSessionClient.class);
+    Call<WorkOut> workOutCall = workOutSessionClient.addSession(
+      year,
+      month,
+      day,
+      location,
+      exercise_type,
+      reps,
+      sets,
+      member
+    );
+    workOutCall.enqueue(new Callback<WorkOut>() {
+      @Override
+      public void onResponse(@NonNull Call<WorkOut> call, @NonNull Response<WorkOut> response) {
+        if(response.isSuccessful()) {
+          Toast.makeText(AddWorkOutSessionActivity.this, "Session Added Successfully", Toast.LENGTH_SHORT).show();
         }
+      }
+
+      @Override
+      public void onFailure(@NonNull Call<WorkOut> call, @NonNull Throwable t) {
+        Log.d(TAG, t.toString());
+      }
+    });
+  }
+
+  public void loadLocationsSpinner() {
+    GymLocationClient gymLocationClient = APIServiceProvider.createService(GymLocationClient.class);
+    Call<List<GymLocation>> gymLocationCall = gymLocationClient.gymLocations();
+
+    gymLocationCall.enqueue(new Callback<List<GymLocation>>() {
+      @Override
+      public void onResponse(@NonNull Call<List<GymLocation>> call, @NonNull Response<List<GymLocation>> response) {
+        gymLocationsList = response.body();
+        addLocationsToArrayList();
+        locationsSpinnerListener();
+      }
+
+      @Override
+      public void onFailure(@NonNull Call<List<GymLocation>> call, @NonNull Throwable t) {
+        Log.d(TAG, t.toString());
+      }
+    });
+  }
+
+  public void loadExercisesSpinner() {
+    ExerciseClient exerciseClient = APIServiceProvider.createService(ExerciseClient.class);
+    Call<List<Exercise>> exercisesCall = exerciseClient.exercises();
+
+    exercisesCall.enqueue(new Callback<List<Exercise>>() {
+      @Override
+      public void onResponse(@NonNull Call<List<Exercise>> call, @NonNull Response<List<Exercise>> response) {
+        exerciseTypesList = response.body();
+        addExercisesToArrayList();
+        exercisesSpinnerListener();
+        loading.setVisibility(View.GONE);
+      }
+
+      @Override
+      public void onFailure(@NonNull Call<List<Exercise>> call, @NonNull Throwable t) {
+        Log.d(TAG, t.toString());
+      }
+    });
+  }
+
+  public void addLocationsToArrayList() {
+
+    for (GymLocation gymLocation : gymLocationsList ) {
+      locations.add(gymLocation.getLocation());
+      locationMap.put(gymLocation.getLocation(), gymLocation.getId());
     }
+  }
 
-    public void addExercisesToArrayList() {
-        for(Exercise exercise : exerciseTypesList) {
-            exercises.add(exercise.getName());
-            exerciseMap.put(exercise.getName(), exercise.getId());
-        }
+  public void addExercisesToArrayList() {
+    for(Exercise exercise : exerciseTypesList) {
+      exercises.add(exercise.getName());
+      exerciseMap.put(exercise.getName(), exercise.getId());
     }
+  }
 
-    public void locationsSpinnerListener() {
-        locationsAdapter = new ArrayAdapter<>(this, R.layout.support_simple_spinner_dropdown_item, locations);
-        sessionLocationsSpinner.setAdapter(locationsAdapter);
+  public void locationsSpinnerListener() {
+    locationsAdapter = new ArrayAdapter<>(this, R.layout.support_simple_spinner_dropdown_item, locations);
+    sessionLocationsSpinner.setAdapter(locationsAdapter);
 
-        sessionLocationsSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-            @Override
-            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-                String locationKey = (String) parent.getItemAtPosition(position);
-                location = locationMap.get(locationKey);
-            }
+    sessionLocationsSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+      @Override
+      public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        String locationKey = (String) parent.getItemAtPosition(position);
+        location = locationMap.get(locationKey);
+      }
 
-            @Override
-            public void onNothingSelected(AdapterView<?> parent) {
-                Log.d(TAG, "Hey! Select a location!");
-            }
-        });
-    }
+      @Override
+      public void onNothingSelected(AdapterView<?> parent) {
+        Log.d(TAG, "Hey! Select a location!");
+      }
+    });
+  }
 
-    public void exercisesSpinnerListener() {
-        exercisesAdapter = new ArrayAdapter<>(this, R.layout.support_simple_spinner_dropdown_item, exercises);
-        exerciseTypeSpinner.setAdapter(exercisesAdapter);
+  public void exercisesSpinnerListener() {
+    exercisesAdapter = new ArrayAdapter<>(this, R.layout.support_simple_spinner_dropdown_item, exercises);
+    exerciseTypeSpinner.setAdapter(exercisesAdapter);
 
-        exerciseTypeSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-            @Override
-            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-                String exerciseKey = (String) parent.getItemAtPosition(position);
-                exercise_type = exerciseMap.get(exerciseKey);
-            }
+    exerciseTypeSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+      @Override
+      public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        String exerciseKey = (String) parent.getItemAtPosition(position);
+        exercise_type = exerciseMap.get(exerciseKey);
+      }
 
-            @Override
-            public void onNothingSelected(AdapterView<?> parent) {
-                Log.d(TAG, "Hey! Select an exercise!");
-            }
-        });
-    }
+      @Override
+      public void onNothingSelected(AdapterView<?> parent) {
+        Log.d(TAG, "Hey! Select an exercise!");
+      }
+    });
+  }
 
-    void hideKeyboard() {
-        InputMethodManager inputMethodManager = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
-        inputMethodManager.hideSoftInputFromWindow(getCurrentFocus().getWindowToken(), InputMethodManager.HIDE_NOT_ALWAYS);
-    }
+  void hideKeyboard() {
+    InputMethodManager inputMethodManager = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+    inputMethodManager.hideSoftInputFromWindow(getCurrentFocus().getWindowToken(), InputMethodManager.HIDE_NOT_ALWAYS);
+  }
 }
--- app/src/main/java/ke/co/milleradulu/milleradulu/mazoezigymnasium/workoutsessions/history/WorkOutHistoryActivity.java
+++ app/src/main/java/ke/co/milleradulu/milleradulu/mazoezigymnasium/workoutsessions/history/WorkOutHistoryActivity.java
@@ -6,11 +6,15 @@ import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
+import android.view.View;
+import android.widget.ProgressBar;
 
+import java.util.HashMap;
 import java.util.List;
 
 import ke.co.milleradulu.milleradulu.mazoezigymnasium.R;
 import ke.co.milleradulu.milleradulu.mazoezigymnasium.APIServiceProvider;
+import ke.co.milleradulu.milleradulu.mazoezigymnasium.SessionManager;
 import ke.co.milleradulu.milleradulu.mazoezigymnasium.models.WorkOut;
 import ke.co.milleradulu.milleradulu.mazoezigymnasium.clients.WorkOutSessionClient;
 import retrofit2.Call;
@@ -18,38 +22,55 @@ import retrofit2.Callback;
 
 public class WorkOutHistoryActivity extends AppCompatActivity {
 
-    public static final String TAG = WorkOutHistoryActivity.class.getSimpleName();
-    private RecyclerView workOutHistoryRecyclerView;
-    private RecyclerView.Adapter workOutHistoryAdapter;
-    List<WorkOut> workOuts;
+  public static final String TAG = WorkOutHistoryActivity.class.getSimpleName();
+  SessionManager sessionManager;
+  private RecyclerView workOutHistoryRecyclerView;
+  private RecyclerView.Adapter workOutHistoryAdapter;
+  List<WorkOut> workOuts;
+  ProgressBar historyFetch;
+  HashMap<String, String> member;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_work_out_history);
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_work_out_history);
 
-        workOutHistoryRecyclerView = findViewById(R.id.session_history_recycler_view);
-        workOutHistoryRecyclerView.setHasFixedSize(true);
+    sessionManager = new SessionManager(getApplicationContext());
+    sessionManager.checkLogin();
 
-        RecyclerView.LayoutManager workOutsLayoutManager = new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false);
-        workOutHistoryRecyclerView.setLayoutManager(workOutsLayoutManager);
+    member = sessionManager.getMemberDetails();
+    historyFetch = findViewById(R.id.progress);
+    historyFetch.setVisibility(View.VISIBLE);
+    workOutHistoryRecyclerView = findViewById(R.id.session_history_recycler_view);
+    workOutHistoryRecyclerView.setHasFixedSize(true);
 
-        WorkOutSessionClient workOutSessionClient = APIServiceProvider.createService(WorkOutSessionClient.class);
-        Call<List<WorkOut>> workOutCall = workOutSessionClient.workOutSessions(1);
+    RecyclerView.LayoutManager workOutsLayoutManager = new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false);
+    workOutHistoryRecyclerView.setLayoutManager(workOutsLayoutManager);
 
-        workOutCall.enqueue(new Callback<List<WorkOut>>() {
-            @Override
-            public void onResponse(@NonNull Call<List<WorkOut>> call, @NonNull retrofit2.Response<List<WorkOut>> response) {
-                workOuts = response.body();
-                workOutHistoryAdapter = new WorkOutAdapter(workOuts);
-                workOutHistoryRecyclerView.setAdapter(workOutHistoryAdapter);
-            }
+    WorkOutSessionClient workOutSessionClient = APIServiceProvider.createService(WorkOutSessionClient.class);
+    Call<List<WorkOut>> workOutCall = workOutSessionClient.workOutSessions(
+      Integer.parseInt(
+        member.get(
+          SessionManager.KEY_MEMBER_ID
+        )
+      )
+    );
 
-            @Override
-            public void onFailure(@NonNull Call<List<WorkOut>> call, @NonNull Throwable t) {
-                Log.d(TAG, t.toString());
-            }
-        });
+    workOutCall.enqueue(new Callback<List<WorkOut>>() {
+      @Override
+      public void onResponse(@NonNull Call<List<WorkOut>> call, @NonNull retrofit2.Response<List<WorkOut>> response) {
+        historyFetch.setVisibility(View.GONE);
+        workOuts = response.body();
+        workOutHistoryAdapter = new WorkOutAdapter(workOuts);
+        workOutHistoryRecyclerView.setAdapter(workOutHistoryAdapter);
+        workOutHistoryRecyclerView.setVisibility(View.VISIBLE);
+      }
 
-    }
+      @Override
+      public void onFailure(@NonNull Call<List<WorkOut>> call, @NonNull Throwable t) {
+        Log.d(TAG, t.toString());
+      }
+    });
+
+  }
 }
