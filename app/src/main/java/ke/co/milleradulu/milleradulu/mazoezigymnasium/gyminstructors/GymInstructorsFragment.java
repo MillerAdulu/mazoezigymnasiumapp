16ba7047e659ca0fec5738962771bfa32365e7bc
Miller Adulu <milleradulu@gmail.com>
2018-07-06 00:51:45 +0300

    Gym Instructors converted to Fragments

--- app/src/main/java/ke/co/milleradulu/milleradulu/mazoezigymnasium/gyminstructors/GymInstructorsFragment.java
+++ app/src/main/java/ke/co/milleradulu/milleradulu/mazoezigymnasium/gyminstructors/GymInstructorsFragment.java
@@ -3,69 +3,101 @@ package ke.co.milleradulu.milleradulu.mazoezigymnasium.gyminstructors;
 import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.DefaultItemAnimator;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ProgressBar;
+
+import java.util.List;
 
 import ke.co.milleradulu.milleradulu.mazoezigymnasium.R;
+import ke.co.milleradulu.milleradulu.mazoezigymnasium.SessionManager;
+import ke.co.milleradulu.milleradulu.mazoezigymnasium.apihandler.APIHelper;
+import ke.co.milleradulu.milleradulu.mazoezigymnasium.apihandler.APIServiceProvider;
+import ke.co.milleradulu.milleradulu.mazoezigymnasium.apihandler.clients.GymInstructorClient;
+import ke.co.milleradulu.milleradulu.mazoezigymnasium.apihandler.models.GymInstructor;
+import ke.co.milleradulu.milleradulu.mazoezigymnasium.gymlocations.GymLocationsFragment;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
-/**
- * A simple {@link Fragment} subclass.
- * Activities that contain this fragment must implement the
- * {@link GymInstructorsFragment.OnFragmentInteractionListener} interface
- * to handle interaction events.
- * Use the {@link GymInstructorsFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class GymInstructorsFragment extends Fragment {
-  // TODO: Rename parameter arguments, choose names that match
-  // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-  private static final String ARG_PARAM1 = "param1";
-  private static final String ARG_PARAM2 = "param2";
-
-  // TODO: Rename and change types of parameters
-  private String mParam1;
-  private String mParam2;
 
   private OnFragmentInteractionListener mListener;
+
+  public static final String TAG = GymLocationsFragment.class.getSimpleName();
+  List<GymInstructor> gymInstructors;
+  private RecyclerView gymInstructorsRecyclerView;
+  private RecyclerView.Adapter gymInstructorsAdapter;
+
+  SessionManager sessionManager;
+  ProgressBar status;
+
 
   public GymInstructorsFragment() {
     // Required empty public constructor
   }
 
-  /**
-   * Use this factory method to create a new instance of
-   * this fragment using the provided parameters.
-   *
-   * @param param1 Parameter 1.
-   * @param param2 Parameter 2.
-   * @return A new instance of fragment GymInstructorsFragment.
-   */
-  // TODO: Rename and change types and number of parameters
-  public static GymInstructorsFragment newInstance(String param1, String param2) {
-    GymInstructorsFragment fragment = new GymInstructorsFragment();
-    Bundle args = new Bundle();
-    args.putString(ARG_PARAM1, param1);
-    args.putString(ARG_PARAM2, param2);
-    fragment.setArguments(args);
-    return fragment;
+  public static GymInstructorsFragment newInstance() {
+    return new GymInstructorsFragment();
   }
 
   @Override
   public void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
-    if (getArguments() != null) {
-      mParam1 = getArguments().getString(ARG_PARAM1);
-      mParam2 = getArguments().getString(ARG_PARAM2);
-    }
+
   }
 
   @Override
-  public View onCreateView(LayoutInflater inflater, ViewGroup container,
+  public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
                            Bundle savedInstanceState) {
-    // Inflate the layout for this fragment
     return inflater.inflate(R.layout.fragment_gym_instructors, container, false);
+  }
+
+  @Override
+  public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+    super.onViewCreated(view, savedInstanceState);
+
+    sessionManager = new SessionManager(getContext());
+    sessionManager.checkLogin();
+
+    status = view.findViewById(R.id.instuctors_load);
+    showLoading();
+
+    gymInstructorsRecyclerView = view.findViewById(R.id.gym_instructors_recycler_view);
+    gymInstructorsRecyclerView.setHasFixedSize(true);
+
+
+    RecyclerView.LayoutManager gymInstructorsLayoutManager = new LinearLayoutManager(getContext(), LinearLayoutManager.VERTICAL, false);
+    gymInstructorsRecyclerView.setLayoutManager(gymInstructorsLayoutManager);
+    gymInstructorsRecyclerView.setItemAnimator(new DefaultItemAnimator());
+
+
+    GymInstructorClient gymInstructorClient = APIServiceProvider.createService(GymInstructorClient.class);
+    Call<List<GymInstructor>> gymInstructorCall = gymInstructorClient.gymInstructors();
+
+    APIHelper.enqueWithRetry(gymInstructorCall, 3, new Callback<List<GymInstructor>>() {
+      @Override
+      public void onResponse(@NonNull Call<List<GymInstructor>> call, @NonNull Response<List<GymInstructor>> response) {
+        gymInstructors = response.body();
+        gymInstructorsAdapter = new GymInstructorAdapter(gymInstructors, getContext());
+        gymInstructorsRecyclerView.setAdapter(gymInstructorsAdapter);
+        stopLoading();
+      }
+
+      @Override
+      public void onFailure(@NonNull Call<List<GymInstructor>> call, @NonNull Throwable t) {
+        Log.d(TAG, t.getMessage());
+      }
+    });
+
   }
 
   // TODO: Rename method, update argument and hook method into UI event
@@ -92,18 +124,15 @@ public class GymInstructorsFragment extends Fragment {
     mListener = null;
   }
 
-  /**
-   * This interface must be implemented by activities that contain this
-   * fragment to allow an interaction in this fragment to be communicated
-   * to the activity and potentially other fragments contained in that
-   * activity.
-   * <p>
-   * See the Android Training lesson <a href=
-   * "http://developer.android.com/training/basics/fragments/communicating.html"
-   * >Communicating with Other Fragments</a> for more information.
-   */
+  void showLoading() {
+    status.setVisibility(View.VISIBLE);
+  }
+
+  void stopLoading() {
+    status.setVisibility(View.GONE);
+  }
+
   public interface OnFragmentInteractionListener {
-    // TODO: Update argument type and name
     void onFragmentInteraction(Uri uri);
   }
 }
